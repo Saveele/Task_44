c3fed4785066def83e45623cdf2eb8702688961b
Elena <savelyevahelenv@gmail.com>
2023-12-17 14:12:47 +0300

    переведен код DataFrame в one hot без get_dummies

--- Task44.py
+++ Task44.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import random
+
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
+
+data.loc[data.whoAmI == 'robot', 'robot'] = 1
+
+data.loc[data.whoAmI == 'human', 'robot'] = 0
+
+data.loc[data.whoAmI == 'human', 'human'] = 1
+
+data.loc[data.whoAmI == 'robot', 'human'] = 0
+
+data.drop('whoAmI', axis= 1 , inplace= True )
+
+print(data)
